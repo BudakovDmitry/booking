0026bafefbbbd63e714056dd2a4687966774751f
Budakov Dmitry <budakov.it@gmail.com>
2023-04-26 00:38:24 +0300

    Add dotenv

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
-import { API } from "src/constants";
+import { API_URL } from "src/constants";
 
-export const get = (endpoint: string = '') => axios.get(`${API}${endpoint}`);
+export const get = (endpoint: string = '') => axios.get(`${API_URL}${endpoint}`);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   mode: 'development',
@@ -38,6 +39,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
+    new Dotenv(),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.png'],
